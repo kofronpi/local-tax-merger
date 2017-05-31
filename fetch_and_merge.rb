8f31fc40318bf697d6eb1f382379b9b6c4fa5b15
Pierre-Alexandre Kofron <pierrealexandre.kofron@gmail.com>
2017-05-31 16:38:33 +0200

    Aggregate data in one csv for each cat except TaxesAnnexes

--- fetch_and_merge.rb
+++ fetch_and_merge.rb
@@ -8,7 +8,7 @@ def create_dir(path)
   FileUtils::mkdir_p(path) unless Dir.exist?(path)
 end
 
-departements = ((1..95).map(&:to_s) - ['20'] << "2b" << "2a" << (971..976).map(&:to_s)).flatten!
+departements = ((1..95).map(&:to_s) - ['20'] << "2b" << "2a" << (971..976).map(&:to_s)).flatten! - ['975']
 departements.map! { |d| d.rjust(2, '0') }
 
 # Get all XLS files
@@ -33,14 +33,18 @@ end
 departements.each do |d|
   system("ssconvert -S tmp/rei_#{YEAR}_#{d}.xls tmp/rei_#{YEAR}_#{d}_%s.csv")
   # Add departement code and trailing zeroes to commune code in first column for each csv, for each category
-  # TH
-  `awk 'BEGIN{FS=OFS=\",\"} NR> 6 { $1=sprintf(\"%03d\", $1);$1=\"#{dd}\"$1}1' tmp/rei_#{YEAR}_#{dd}_TH.csv > tmp/rei_#{YEAR}_#{dd}_TH_clean.csv`
+  #
+  %w(TH FB FNB CFE).each do |cat|
+    `awk 'BEGIN{FS=OFS=\",\"} NR> 6 { $1=sprintf(\"%03d\", $1);$1=\"#{d.upcase}\"$1}1' tmp/rei_#{YEAR}_#{d}_#{cat}.csv > tmp/rei_#{YEAR}_#{d}_#{cat}_clean.csv`
+  end
+  # TaxesAnnexes : later
 end
 
-dd = '01'
 create_dir('./output')
-# Merge all CSVs for TH into one
-# there is a 2-line count error probably because of wrong delimiter in origin csv files
-system("awk 'FNR < 7{next;}{print}' *_TH_clean.csv > ./output/taxe_habitation.csv")
 
-# Remove all in tmp folder
+# Merge all CSVs for each cat into one
+# there is a 2-line count error probably because of wrong delimiter in origin csv files
+system("awk 'FNR < 7{next;}{print}' ./tmp/*_TH_clean.csv > ./output/taxe_habitation.csv")
+system("awk 'FNR < 7{next;}{print}' ./tmp/*_FB_clean.csv > ./output/taxe_fonciere_bati.csv")
+system("awk 'FNR < 7{next;}{print}' ./tmp/*_FNB_clean.csv > ./output/taxe_fonciere_non_bati.csv")
+system("awk 'FNR < 7{next;}{print}' ./tmp/*_CFE_clean.csv > ./output/taxe_cfe.csv")
